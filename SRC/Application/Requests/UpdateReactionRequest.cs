c03b304521d262895ffba55d4282f99a9ebcdfb5
EseniyaIvanovna <eseniamizhutina@yandex.ru>
2025-03-29 21:45:28 +0300

    hw-5 fix requests

--- SRC/Application/Requests/UpdateReactionRequest.cs
+++ SRC/Application/Requests/UpdateReactionRequest.cs
@@ -6,8 +6,6 @@ namespace Application.Requests
     public class UpdateReactionRequest
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int PostId { get; set; }
         public ReactionType Type { get; set; }
     }
     public class UpdateReactionRequestValidator : AbstractValidator<UpdateReactionRequest>
@@ -17,15 +15,7 @@ namespace Application.Requests
             RuleFor(x => x.Id)
                 .NotEmpty().WithMessage("Reaction ID is required")
                 .GreaterThanOrEqualTo(0).WithMessage("Reaction ID must be positive");
-
-            RuleFor(x => x.UserId)
-                .NotEmpty().WithMessage("User ID is required")
-                .GreaterThanOrEqualTo(0).WithMessage("User ID must be positive");
-
-            RuleFor(x => x.PostId)
-                .NotEmpty().WithMessage("Post ID is required")
-                .GreaterThanOrEqualTo(0).WithMessage("Post ID must be positive");
-
+            
             RuleFor(x => x.Type)
                 .IsInEnum().WithMessage("Invalid reaction type");
         }
