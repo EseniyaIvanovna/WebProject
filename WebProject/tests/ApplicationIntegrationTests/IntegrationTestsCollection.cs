f59556a4f8d26d6d4a3ff23f3f301cb2be51ff13
EseniyaIvanovna <eseniamizhutina@yandex.ru>
2025-04-19 16:14:04 +0300

    change folder

--- WebProject/tests/ApplicationIntegrationTests/IntegrationTestsCollection.cs
+++
@@ -1,6 +0,0 @@
-﻿using ApplicationIntegrationTests;
-
-namespace ApplicationIntegrationsTests;
-
-[CollectionDefinition("IntegrationTests")]
-public class IntegrationTestsCollection : ICollectionFixture<TestingFixture> { }
